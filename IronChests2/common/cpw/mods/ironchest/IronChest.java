c63b0560289fe16645f380494cd3b50cefeb871f
Christian <cpw@weeksfamily.ca>
2012-10-23 19:18:36 -0400

    Update for forge 6.0

--- IronChests2/common/cpw/mods/ironchest/IronChest.java
+++ IronChests2/common/cpw/mods/ironchest/IronChest.java
@@ -29,8 +29,8 @@ import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
-@Mod(modid = "IronChest", name = "Iron Chests", dependencies="required-after:FML@[4.0,)")
-@NetworkMod(channels = { "IronChest" }, versionBounds = "[4.1,)", clientSideRequired = true, serverSideRequired = false, packetHandler = PacketHandler.class)
+@Mod(modid = "IronChest", name = "Iron Chests", dependencies="required-after:FML@[6.0,)")
+@NetworkMod(channels = { "IronChest" }, versionBounds = "[4.2,)", clientSideRequired = true, serverSideRequired = false, packetHandler = PacketHandler.class)
 public class IronChest {
 	public static BlockIronChest ironChestBlock;
 	@SidedProxy(clientSide = "cpw.mods.ironchest.client.ClientProxy", serverSide = "cpw.mods.ironchest.CommonProxy")
