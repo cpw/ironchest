0aae1ff3347b3d59870c0459bf8e24faf641c1fb
Alexander <traincrazyb@live.com>
2016-03-26 14:47:15 -0400

    Fix #64, only call the event on the client.

--- src/main/java/cpw/mods/ironchest/client/IronChestEventHandler.java
+++ src/main/java/cpw/mods/ironchest/client/IronChestEventHandler.java
@@ -4,12 +4,15 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
 
 public class IronChestEventHandler
 {
     public static IronChestEventHandler INSTANCE = new IronChestEventHandler();
 
     @SubscribeEvent
+    @SideOnly(Side.CLIENT)
     public void onPreTextureStiching(TextureStitchEvent.Pre event)
     {
         if (event.getMap() == FMLClientHandler.instance().getClient().getTextureMapBlocks())
