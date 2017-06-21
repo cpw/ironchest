1efea0cc6b4e86e05263110af8bf15f95bab3acd
alexbegt <traincrazyb@aol.com>
2017-06-21 13:43:58 -0400

    Require Forge Version 14.21.0.2343 which fixes the crash regarding recipes. Closes #98

--- src/main/java/cpw/mods/ironchest/IronChest.java
+++ src/main/java/cpw/mods/ironchest/IronChest.java
@@ -30,7 +30,7 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = IronChest.MOD_ID, name = "Iron Chests", dependencies = "required-after:forge@[14.21.0.2333,)", acceptedMinecraftVersions = "[1.12, 1.13)")
+@Mod(modid = IronChest.MOD_ID, name = "Iron Chests", dependencies = "required-after:forge@[14.21.0.2343,)", acceptedMinecraftVersions = "[1.12, 1.13)")
 public class IronChest
 {
     public static final String MOD_ID = "ironchest";
